0f2198aa4b959292c8ee4534b6b07053066933f6
Deivis <deivisutp@gmail.com>
2021-04-25 17:32:24 -0300

    first commit

---
+++ src/utils/convertDurationTimeString.ts
@@ -0,0 +1,11 @@
+export function convertDurationTimeString(duration: number): string {
+    const hours = Math.floor(duration / (60 * 60));
+    const minutes = Math.floor((duration % (60 * 60)) / 60);
+    const seconds = duration % 60;
+
+    const finalResult = [hours, minutes, seconds]
+        .map(unit => String(unit).padStart(2, '0'))
+        .join(':');
+
+    return finalResult;
+}
