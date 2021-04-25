17c3ae86558e4f9b38f0ecec133b49300237602b
Deivis <deivisutp@gmail.com>
2021-04-25 19:42:25 -0300

    Player features

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,4 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
+
 import api from '../../services/api';
 
 import { format, parseISO } from 'date-fns';
@@ -26,6 +28,10 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode }: EpisodeProps) {
+    const router = useRouter();
+
+    if (router.isFallback) return <p>Carregando...</p>
+
     return (
         <div className={styles.episode}>
             <div className={styles.thumbnailContainer}>
@@ -60,8 +66,24 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+    const { data } = await api.get('episodes', {
+        params: {
+            _limit: 2,
+            _sort: 'published_at',
+            _order: 'desc',
+        }
+    });
+
+    const paths = data.map(episode => {
+        return {
+            params: {
+                slug: episode.id
+            }
+        }
+    });
+
     return {
-        paths: [],
+        paths,
         fallback: 'blocking',
     }
 }
